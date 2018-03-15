a0110a24db58710fd50690d6b86d3a2f42176c5d
hanzhongyi <hanzhongyi@baidu.com>
2018-03-15 17:04:45 +0800

    modified:   config/path.cfg 	new file:   practices.py

---
+++ practices.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 15 16:48:43 2018
+
+@author: zhongyi
+"""
+#VOID SPACE FOR FUTURE TESTING!
+from tqdm import tqdm
+for i in tqdm(range(999)):
+    pass
